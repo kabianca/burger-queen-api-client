ad2b4497e78f2071c961ae7f15788570e8d4859e
kabianca <kbiancasol@gmail.com>
2022-10-24 17:39:49 -0300

    Add página de login Pagina de login. Pasta de assets com todos os icones. Readme v.1.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import burgerLogin from "./assets/burger.jpg"
+import background from "./assets/background-login.svg"
+import "../src/App.css"
+
 function App() {
   return (
     <div className="App">
-      <h1>PEBA</h1>
+      <img src={burgerLogin} alt="logo-hamburger" />
+      <form style={{backgroundImage: `url(${background})` }}>
+        <input type="text" placeholder="USUÁRIO" />
+        <input type="password" placeholder="SENHA" />
+        <input type="submit" value="ENTRAR" />
+      </form>
     </div>
   );
 }
