bf95c390a171e9395013aacb63acf960452f9afb
Clarissa Simplício <clasimplicio@hotmail.com>
2022-11-04 03:11:18 -0300

    iniciando o css do menu

--- src/components/buttonProducts/buttonProducts.js
+++ src/components/buttonProducts/buttonProducts.js
@@ -1,9 +1,14 @@
+import "./buttonProducts.css";
+
 const ButtonProducts = ({children}) => {
     return (
-        <>
-            <h1>{children.name}</h1>
-            <h1>{children.price}</h1>
-        </>
+        <section className="card-product">
+            <div className="title-product">
+                <img className="img-product" src={children.image}></img>
+                <p className="name-product">{children.name}</p>
+            </div>
+            <h1 className="price-product">R$ {children.price}</h1>
+        </section>
     )
 }
 
